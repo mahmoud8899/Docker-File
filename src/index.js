cb98337739d14fa0311dbdd49beef98dd765e7cf
Mahmoud Almadhoun <mahmoud.talat899@gmail.com>
2022-11-02 15:28:53 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+
+const data = {
+    name : 'mahmoud',
+    age : 33
+}
+
+app.get('/', (req,res)=> res.status(200).send('hello nginx. ... .........'))
+  
+
+app.get('/app', (req,res)=> res.status(200).send(data))
+  
+
+app.listen(process.env.PORT || 4000, ()=> console.log('server is runs.....',process.env.PORT))
