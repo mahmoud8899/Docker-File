6dcb0848d84dbb0d246524b68900df5a3c22cbea
Mahmoud Almadhoun <mahmoud.talat899@gmail.com>
2022-11-02 16:13:15 +0000

    add docker image

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const os = require('os')
 
 
 const data = {
@@ -8,7 +9,9 @@ const data = {
     age : 33
 }
 
-app.get('/', (req,res)=> res.status(200).send('hello nginx. ... .........'))
+app.get('/', (req,res)=>{
+    res.status(200).send('hello nginx. ... .........')
+})
   
 
 app.get('/app', (req,res)=> res.status(200).send(data))
